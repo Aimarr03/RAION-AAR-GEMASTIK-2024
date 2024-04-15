569137397faae071b59fe24365456bb9ec9c5142
Aimarr03 <andyaimar729@gmail.com>
2024-04-15 15:55:43 +0700

    02-13-Create dynamic Card (RecyclerView-like) for SetUp UI

--- AAR-GEMASTIK-2024/Assets/Scripts/UI/SetUp/SetUpUI.cs
+++ AAR-GEMASTIK-2024/Assets/Scripts/UI/SetUp/SetUpUI.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class SetUpUI : BasePreparingPlayerUI
 {
-    
+    [SerializeField] private Transform templateCard;
+    [SerializeField] private ShopManager shopManager;
+    [SerializeField] private Transform weaponContainer, abilityContainer, itemContainer;
+
+    private void Awake()
+    {
+        SetUp();
+    }
+    private void SetUp()
+    {
+        Transform currentCard;
+        foreach(ItemBaseSO currentWeaponSO in shopManager.weaponList)
+        {
+            currentCard = Instantiate(templateCard, weaponContainer);
+            currentCard.gameObject.SetActive(true);
+        }
+        foreach (ItemBaseSO currentAbilitySO in shopManager.abilityList)
+        {
+            currentCard = Instantiate(templateCard, abilityContainer);
+            currentCard.gameObject.SetActive(true);
+        }
+        foreach (ItemBaseSO currentItemSO in shopManager.weaponList)
+        {
+            currentCard = Instantiate(templateCard, itemContainer);
+            currentCard.gameObject.SetActive(true);
+        }
+    }
 }
