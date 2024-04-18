54ae946fa39fa2b6087d98b49ba4fd67837d64af
Aimarr03 <andyaimar729@gmail.com>
2024-04-18 22:08:39 +0700

    02-46-Setting Up Consumable System

--- AAR-GEMASTIK-2024/Assets/Scripts/Player/PlayerConsumptionItemSystem.cs
+++ AAR-GEMASTIK-2024/Assets/Scripts/Player/PlayerConsumptionItemSystem.cs
@@ -5,8 +5,45 @@ using UnityEngine;
 public class PlayerConsumptionItemSystem : MonoBehaviour
 {
     private PlayerCoreSystem coreSystem;
+    [SerializeField] private ConsumableItemSO healthConsumptionSO;
+    [SerializeField] private ConsumableItemSO oxygenConsumptionSO;
+    [SerializeField] private ConsumableItemSO energyConsumptionSO;
+    private Dictionary<SustainabilityType, ConsumableItemSO> ListConsumableSO;
+    private int maxIndex = 2;
+    private int currentIndex = 0;
     private void Awake()
     {
         coreSystem = GetComponent<PlayerCoreSystem>();
+        ListConsumableSO = new Dictionary<SustainabilityType, ConsumableItemSO>
+        {
+            { SustainabilityType.Health, healthConsumptionSO },
+            { SustainabilityType.Oxygen, oxygenConsumptionSO },
+            { SustainabilityType.Energy, oxygenConsumptionSO}
+        };
+
+    }
+
+    private SustainabilityType GetSustainabilityTypeBasedOnIndex()
+    {
+        switch (currentIndex)
+        {
+            case 0: return SustainabilityType.Health;
+            case 1: return SustainabilityType.Oxygen;
+            case 2: return SustainabilityType.Energy;
+            default: return SustainabilityType.Capacity;
+        }
+    }
+    public ConsumableItemSO GetConsumableItemSO(SustainabilityType type)
+    {
+        return ListConsumableSO[type];
+    }
+    public ConsumableItemSO GetConsumableItemSO()
+    {
+        return ListConsumableSO[GetSustainabilityTypeBasedOnIndex()];
+    }
+    private void onIncreaseIndex()
+    {
+        currentIndex++;
+        if (currentIndex > maxIndex) currentIndex = 0;
     }
 }
