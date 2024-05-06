064126f5fa87037be85046ec307cb546034bebeb
Aimarr03 <andyaimar729@gmail.com>
2024-05-06 14:15:11 +0700

    02-83-Added UI for SetUP UI

--- AAR-GEMASTIK-2024/Assets/Scripts/UI/SetUp/SetUpCard.cs
+++ AAR-GEMASTIK-2024/Assets/Scripts/UI/SetUp/SetUpCard.cs
@@ -9,19 +9,39 @@ public class SetUpCard : MonoBehaviour
     public Button thisButton;
     public Image background;
     public Image backgroundFocus;
+    public Image unavailableIcon;
+    public Image icon;
     public PlayerUsableGeneralData generalData
     {
         get => itemBaseSO.generalData;
     }
     public ItemType type;
     public TextMeshProUGUI textHeader;
+    public TextMeshProUGUI textLevel;
+    public Color unavailableColor;
     public static event Action<ItemBaseSO> onChoseItem;
     public void SetUpData(ItemBaseSO so, ItemType type)
     {
         this.itemBaseSO = so;
         this.type = type;
-        textHeader.text = generalData.name;
+        string headerText = generalData.name;
+        string levelText = "lvl " + generalData.level.ToString();
+        if (itemBaseSO is ConsumableItemSO consumableItemSO)
+        {
+            headerText += $" {consumableItemSO.itemTier}";
+            levelText = $"Qty: {consumableItemSO.quantity}";
+        }
+        textHeader.text = headerText;
+        textLevel.text = levelText;
+        if(itemBaseSO.generalData.icon != null)
+        {
+            icon.sprite = itemBaseSO.generalData.icon;
+        }
         thisButton.interactable = generalData.unlocked;
+        textLevel.color = generalData.unlocked? textLevel.color : unavailableColor;
+        textHeader.color = generalData.unlocked ? textHeader.color : unavailableColor;
+        
+        unavailableIcon.gameObject.SetActive(generalData.unlocked);
         backgroundFocus.gameObject.SetActive(false);
         UpdateData();
     }
